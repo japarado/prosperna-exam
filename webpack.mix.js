f21548ea88e989456a58102e570f14d21988e0fe
pam <justin.parado.personal@gmail.com>
2020-12-28 22:56:57 +0800

    Eslint package installation

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,13 +11,13 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-	.postCss('resources/css/app.css', 'public/css', [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-])
-	.sourceMaps(true, 'source-map')
+mix.js("resources/js/app.js", "public/js")
+	.postCss("resources/css/app.css", "public/css", [
+		require("postcss-import"),
+		require("tailwindcss"),
+		require("autoprefixer"),
+	])
+	.sourceMaps(true, "source-map")
 	.browserSync({
 		proxy: "https://prosperna-exam.apc",
 		open: false
