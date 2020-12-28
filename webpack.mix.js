6802ae8b6135c64e426f73769680efbf62e89786
pam <justin.parado.personal@gmail.com>
2020-12-29 00:27:30 +0800

    Process js files for test pages

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,9 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.js("resources/js/app.js", "public/js")
+mix
+	.js("resources/js/app.js", "public/js")
+	.js("resources/js/pages/test/index.js", "public/js")
 	.postCss("resources/css/app.css", "public/css", [
 		require("postcss-import"),
 		require("tailwindcss"),
