8c648e2971532e4dbd88fb95f2f76e3da8dfd836
pam <justin.parado.personal@gmail.com>
2020-12-28 23:44:49 +0800

    Restored postcss processing

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,12 +12,11 @@ const mix = require("laravel-mix");
  */
 
 mix.js("resources/js/app.js", "public/js")
-	.sass("resources/css/app.scss", "public/css")
-	// .postCss("resources/css/app.css", "public/css", [
-	// 	require("postcss-import"),
-	// 	require("tailwindcss"),
-	// 	require("autoprefixer"),
-	// ])
+	.postCss("resources/css/app.css", "public/css", [
+		require("postcss-import"),
+		require("tailwindcss"),
+		require("autoprefixer"),
+	])
 	.sourceMaps(true, "source-map")
 	.browserSync({
 		proxy: "https://prosperna-exam.apc",
