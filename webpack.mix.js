d307c1e52075687a8d4161d0a5bee0ab75a5ecdc
pam <justin.parado.personal@gmail.com>
2020-12-28 22:46:33 +0800

    Webpack configs for auto reload

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,14 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
+mix.js('resources/js/app.js', 'public/js')
+	.postCss('resources/css/app.css', 'public/css', [
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
-]);
+])
+	.sourceMaps(true, 'source-map')
+	.browserSync({
+		proxy: "https://prosperna-exam.apc",
+		open: false
+	});
