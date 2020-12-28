370c5c07a873661b2501bcc649f106b6d58c86e8
pam <justin.parado.personal@gmail.com>
2020-12-28 23:22:34 +0800

    SCSS preprocessing for assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,11 +12,12 @@ const mix = require("laravel-mix");
  */
 
 mix.js("resources/js/app.js", "public/js")
-	.postCss("resources/css/app.css", "public/css", [
-		require("postcss-import"),
-		require("tailwindcss"),
-		require("autoprefixer"),
-	])
+	.sass("resources/css/app.scss", "public/css")
+	// .postCss("resources/css/app.css", "public/css", [
+	// 	require("postcss-import"),
+	// 	require("tailwindcss"),
+	// 	require("autoprefixer"),
+	// ])
 	.sourceMaps(true, "source-map")
 	.browserSync({
 		proxy: "https://prosperna-exam.apc",
