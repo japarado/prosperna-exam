eb777e0bd7a616ed73e831ade77e80cfc657c5c2
pam <justin.parado.personal@gmail.com>
2020-12-29 10:15:11 +0800

    Script and style yields in guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+		@yield('guest-layouts.styles')
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
@@ -21,4 +22,5 @@
             {{ $slot }}
         </div>
     </body>
+		@yield('guest-layouts.scripts')
 </html>
