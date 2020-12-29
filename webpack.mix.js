7f53891f1d87ceeea3021c539d7ddf1ee4e6d7d8
pam <justin.parado.personal@gmail.com>
2020-12-29 10:15:42 +0800

    Webpack mix entry for test index page

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,7 @@ const mix = require("laravel-mix");
 
 mix
 	.js("resources/js/app.js", "public/js")
-	.js("resources/js/pages/test/index.js", "public/js")
+	.js("resources/js/pages/tests/index.js", "public/js")
 	.postCss("resources/css/app.css", "public/css", [
 		require("postcss-import"),
 		require("tailwindcss"),
