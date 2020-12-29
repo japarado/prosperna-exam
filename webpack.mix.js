f9f9601172bd04cfcb392bd0e3761252524bd417
pam <justin.parado.personal@gmail.com>
2020-12-29 17:15:51 +0800

    Corrected JS file output destinations

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,8 +13,8 @@ const mix = require("laravel-mix");
 
 mix
 	.js("resources/js/app.js", "public/js")
-	.js("resources/js/pages/tests/index.js", "public/js")
-	.js("resources/js/pages/auth/register.js", "public/js")
+	.js("resources/js/pages/tests/index.js", "public/js/pages/tests/")
+	.js("resources/js/pages/auth/register.js", "public/js/pages/auth/")
 	.postCss("resources/css/app.css", "public/css", [
 		require("postcss-import"),
 		require("tailwindcss"),
