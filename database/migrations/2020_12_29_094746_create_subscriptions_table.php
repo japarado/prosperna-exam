102a2c25625354b47c75ac35dbc120c246c2f888
pam <justin.parado.personal@gmail.com>
2020-12-29 17:51:51 +0800

    Subscription table migration

---
+++ database/migrations/2020_12_29_094746_create_subscriptions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+			$table->string('name');
+			$table->string('order_id');
+			$table->string('pyament_id')->nullable();
+			$table->string('subscription_id');
+			$table->string('facilitator_access_token');
+
+			$table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
