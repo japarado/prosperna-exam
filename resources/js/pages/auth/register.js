b929c8f8209a999af7550f98ce44e5d55612ce4d
pam <justin.parado.personal@gmail.com>
2020-12-29 17:15:43 +0800

    Initial handler for registration with paypal

--- resources/js/pages/auth/register.js
+++ resources/js/pages/auth/register.js
@@ -1,1 +1,15 @@
-console.log("This is the registration page");
+paypal
+	.Buttons({
+		createSubscription: (data, actions) => 
+		{
+			return actions.subscription.create({
+				"plan_id": "P-7YY87840HY308120GL7VI6XI",
+			});
+		},
+
+		onApprove: (data, actions) => 
+		{
+			alert("Yout have successfully created a subscription", + data.subscriptionID);
+		}
+	})
+	.render("#paypal-button-container");
