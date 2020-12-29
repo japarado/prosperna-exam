ab5a8429266451f139edfbaf9b41a9bba3949af3
pam <justin.parado.personal@gmail.com>
2020-12-29 13:01:14 +0800

    PayPal button creator components

--- resources/views/tests/index.blade.php
+++ resources/views/tests/index.blade.php
@@ -1,5 +1,10 @@
 <x-app-layout>
 	@section('layouts.title', 'Test')
+	@section('layouts.scripts')
+		<script>
+			paypal.Buttons().render("#paypal-button-container")
+		</script>
+	@stop
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Tests') }}
@@ -11,6 +16,8 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
 					This is the test page
+
+					<div id="paypal-button-container"></div>
                 </div>
             </div>
         </div>
