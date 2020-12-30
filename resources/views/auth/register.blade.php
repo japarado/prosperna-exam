b6cfa71f4d6b724337cf724fd9b1436432850618
pam <justin.parado.personal@gmail.com>
2020-12-30 13:05:36 +0800

    Placeholders for registration page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,20 +9,23 @@
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <form  action="{{ route('register') }}" method="POST">
+        <form  action="{{ route('register') }}" method="POST" id="js-registration-form">
+			<input type="hidden" name="paypal-response-hidden" id="js-paypal-response-hidden"/>
 			@csrf
             <!-- Name -->
             <div>
                 <x-label for="name" :value="__('Name')" />
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+                {{-- <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus /> --}}
+                <x-input id="name" class="block mt-1 w-full" type="text" name="name" value="Manta Maria" required autofocus />
             </div>
 
             <!-- Email Address -->
             <div class="mt-4">
                 <x-label for="email" :value="__('Email')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                {{-- <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required /> --}}
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" value="maria@mail.com" required />
             </div>
 
             <!-- Password -->
@@ -32,7 +35,7 @@
                 <x-input id="password" class="block mt-1 w-full"
                                 type="password"
                                 name="password"
-                                required autocomplete="new-password" />
+                                required autocomplete="new-password" value="password123" />
             </div>
 
             <!-- Confirm Password -->
@@ -41,7 +44,7 @@
 
                 <x-input id="password_confirmation" class="block mt-1 w-full"
                                 type="password"
-                                name="password_confirmation" required />
+                                name="password_confirmation" value="password123" required />
             </div>
 
             <div class="flex items-center justify-end mt-4">
@@ -49,13 +52,13 @@
                     {{ __('Already registered?') }}
                 </a>
 
-                <x-button class="ml-4">
+                <x-button class="ml-4" id="js-user-info-submit">
                     {{ __('Register') }}
                 </x-button>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-				<div id="paypal-button-container" class="container"></div>
+            <div class="flex items-center justify-end mt-4 hidden" id="js-paypal-buttons-container">
+				<div id="js-paypal-buttons" class="container"></div>
 			</div>
         </form>
     </x-auth-card>
